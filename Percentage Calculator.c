df5b4964361210cf257ba32e031f02e07aeeda9b
Jeet1243 <72684295+Jeet1243@users.noreply.github.com>
2020-10-11 13:35:16 +0530

    Create Percentage Calculator.c
    This is a simple program to find the percentage of numbers.
    Please try it once.

---
+++ Percentage Calculator.c
@@ -0,0 +1,16 @@
+#https://www.facebook.com/jeet/posts/2729372946102639
+# Subscribed by Code House
+
+#include <stdio.h>
+
+int main() {
+   float percentage;
+   int total_marks = 1200;
+   int scored = 1122;
+
+   percentage = (float)scored / total_marks * 100.0;
+
+   printf("Percentage = %.2f%%", percentage);
+
+   return 0;
+}
